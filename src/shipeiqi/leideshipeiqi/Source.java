8dda1b1f747bc2ce32ebd95bc92228e183ace92a
longchong <116294>
2019-07-05 15:22:09 +0800

    设计模式

---
+++ src/shipeiqi/leideshipeiqi/Source.java
@@ -0,0 +1,10 @@
+package shipeiqi.leideshipeiqi;
+
+public class Source {
+
+    public void method1() {
+
+        System.out.println("this is original method!");
+
+    }
+}
--- src/yuanxing/Prototype2.java
+++ src/yuanxing/Prototype2.java
@@ -87,8 +87,6 @@ public class Prototype2 implements Cloneable, Serializable {
 
     }
 
-
-
 }
 
 
